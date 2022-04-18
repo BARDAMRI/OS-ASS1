92b6f0a99fb082da14859491d51bbb2c016568f9
BARDAMRI <bardam@post.bgu.ac.il>
2022-04-18 20:12:04 +0300

    task 1

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -95,3 +95,17 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+uint64
+sys_pause_system(void)
+{
+  int sec;
+
+  if(argint(0, &sec) < 0)
+    return -1;
+  return pause_system(sec);
+}
+uint64
+sys_pause_system(void)
+{
+  return kill_system();
+}
